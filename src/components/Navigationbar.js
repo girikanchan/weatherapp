77be7a1cf7313ac28d7980a1df552a6ef1c6279f
girikanchan <akira.giri2001@gmail.com>
2023-11-17 21:44:40 +0530

    navbar css

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -6,8 +6,8 @@ function Navigationbar() {
     <div className="App">
       <img src={logo} alt="logo"></img>
       <h1>Weather 99</h1>
-      
-      <h5><i className="fa-solid fa-rotate-right"></i>refresh</h5>
+      <i className="fa-solid fa-rotate-right"></i>
+      <h5>refresh</h5>
     </div>
  );
 }
